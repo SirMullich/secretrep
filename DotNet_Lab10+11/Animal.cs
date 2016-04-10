767d90cb8b1f00322348291cb27dcb82c058cabf
SirMullich <daulet@gmail.com>
2016-04-10 10:04:04 +0600

    added Program.cs

---
+++ DotNet_Lab10+11/Animal.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace DotNet_Lab10_11
+{
+    abstract class Animal
+    {
+        public int Legs { get; set; }
+        public int Size { get; set; }
+        
+        abstract public string MakeSound();
+    }
+
+    class Lion : Animal
+    {
+        public string SkinColor { get; set; }
+        
+        public Lion()
+        {
+            Legs = 4;
+            Size = 10;
+            SkinColor = "Yellow";
+        }
+        public override string MakeSound()
+        {
+            return "Rghfhrhghgh!!! I'm a Lion";
+        }
+        public string Kill()
+        {
+            return "Killing!";
+        }
+    }
+
+    class Elephant : Animal
+    {
+        public string Trunk { get; set; }
+        public Elephant()
+        {
+            Legs = 4;
+            Size = 50;
+            Trunk = "I have a trunk";
+        }
+        public override string MakeSound()
+        {
+            return "Muahahauahahaa!!! I'm an elephant";
+        }
+    }
+
+    class Monkey : Animal
+    {
+        public Monkey()
+        {
+            Legs = 2;
+            Size = 5;
+        }
+        public override string MakeSound()
+        {
+            return "Wo-wo-oo-oa-aha-aaa! I'm a monkey";
+        }
+        public string Play()
+        {
+            return "I'm playing!";
+        }
+    }
+
+    class Zoo
+    {
+        public void GetSound<T>()
+        {
+            Type t = typeof(T);
+            MethodInfo[] mi = t.GetMethods();
+            Console.WriteLine("The {0} has the following methods: ", t.FullName);
+            foreach (var method in mi)
+            {
+                Console.WriteLine("{0} returns {1}", method.Name, method.ReturnType);
+            }
+
+            Console.WriteLine("{0}The {1} has the following properties: ", Environment.NewLine, t.FullName);
+
+            PropertyInfo[] pi = t.GetProperties();
+            foreach (var property in pi)
+            {
+                Console.WriteLine("Property {0} is of type {1}", property.Name, property.PropertyType);
+            }
+
+            Console.WriteLine("{0}Invoking methods of {1}: ", Environment.NewLine, t.FullName);
+
+            foreach (MethodInfo method in mi)
+            {
+                if (!(method.Name.StartsWith("s") || method.Name.StartsWith("g") || method.Name.StartsWith("Get") || method.Name.StartsWith("Equal") || method.Name.StartsWith("ToStrin")))
+                {
+                    //Console.WriteLine(method.Name);
+                    Console.WriteLine(method.Invoke(Activator.CreateInstance(t), null));
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ DotNet_Lab10+11/Program.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet_Lab10_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task1();
+            //Task2();
+            Task3();
+
+        }
+        static void Task1()
+        {
+            Zoo z = new Zoo();
+            z.GetSound<Elephant>();
+            z.GetSound<Lion>();
+            z.GetSound<Monkey>();
+        }
+        static void Task2()
+        {
+            Order order = new Order();
+            order.customerPhone = "8 000 88";
+            order.customerStreet = "Al-Farabi, 89";
+
+            var result = Validator.TryValidateObject(order, new ValidationContext(order, null, null), null, true);
+            Console.WriteLine(result);
+
+            order.customerPhone = "+7 (123) 458 0325";
+            result = Validator.TryValidateObject(order, new ValidationContext(order, null, null), null, true);
+            Console.WriteLine(result);
+
+            order.customerPhone = "asdfa sdf 56";
+            result = Validator.TryValidateObject(order, new ValidationContext(order, null, null), null, true);
+            Console.WriteLine(result);
+
+            order.customerPhone = "+7 (689) 789 6542";
+            order.customerStreet = "Abai, haha";
+            result = Validator.TryValidateObject(order, new ValidationContext(order), null, true);
+            Console.WriteLine(result);
+        }
+        static void Task3()
+        {
+            // The path where the files we work with are located
+            string path = @"D:\Dev\Test";
+            DirectoryInfo dir = new DirectoryInfo(path);
+
+            // A list to store our info about files from path directory
+            List<FileSystemInfo> files = new List<FileSystemInfo>();
+            files.AddRange(dir.GetFiles());
+
+
+            // index of current selected file
+            int index = 0;
+
+            // The process which we start when open a file
+            Process myProcess = null;
+
+            while (true)
+            {
+                // Draw the files
+                for (int i = 0; i < files.Count; i++)
+                {
+                    // Only the selected file has different background and foreground color
+                    if (i == index)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.DarkBlue;
+                    }
+                    // All other files look the same
+                    Console.WriteLine(files[i].Name);
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+
+                // Draw the constant lower part
+                Console.WriteLine(Environment.NewLine);
+                Console.WriteLine("----------------------------");
+                Console.WriteLine("Press Up/Down arrow keys to switch between the files");
+                Console.WriteLine("Press F5 to open the selected file");
+                Console.WriteLine("Press F6 to close the opened file");
+
+                // Now we wait for user input
+                ConsoleKeyInfo keyPressed = Console.ReadKey();
+
+                switch (keyPressed.Key)
+                {
+                    // The selected file changes to one above it unless it is already at the very top
+                    case ConsoleKey.UpArrow:
+                        if (index > 0)
+                        {
+                            index--;
+                        }
+                        break;
+                    // Similar to UpArrow
+                    case ConsoleKey.DownArrow:
+                        if (index < files.Count - 1)
+                        {
+                            index++;
+                        }
+                        break;
+                    // Open the file depending on the extension of the file
+                    case ConsoleKey.F5:
+                        try
+                        {
+                            if (Extsn(files[index].Name) == "txt")
+                            {
+                                myProcess = Process.Start("notepad.exe", files[index].FullName);
+                            }
+                            else if (Extsn(files[index].Name) == "jpg")
+                            {
+                                // Couldn't find .exe of default picture viewer :((
+                                myProcess = Process.Start("mspaint.exe", files[index].FullName);
+                            }
+                            else if (Extsn(files[index].Name) == "pdf")
+                            {
+                                // Acrobat Reader
+                                myProcess = Process.Start("AcroRd32.exe", files[index].FullName);
+                            }
+                            else if (Extsn(files[index].Name) == "html")
+                            {
+                                myProcess = Process.Start("firefox.exe", files[index].FullName);
+                            }
+                        }
+                        catch (InvalidOperationException ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+                    // Close the File
+                    case ConsoleKey.F6:
+                        try
+                        {
+                            myProcess.Kill();
+                        }
+                        catch (InvalidOperationException ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+                }
+                // Clear the console before new cycle
+                Console.Clear();
+            }
+        }
+
+        // This method returns the file extension of the received file name
+        public static string Extsn(string fileName)
+        {
+            string result = "";
+            for (int i = fileName.Length - 1; i >= 0; i--)
+            {
+                if (fileName[i] == '.')
+                {
+                    break;
+                }
+                else
+                {
+                    result = fileName[i] + result;
+                }   
+            }
+            return result;
+        }
+    }
+}
